e8667a2068158febbb13453ab32c4fe2a2a62230
Galder Zamarreño <galder@redhat.com>
2021-01-06 09:01:23 +0100

    Move to package hello example, stop using JBang

---
+++ hello/example/Example.java
@@ -0,0 +1,35 @@
+package example;
+
+import java.lang.System;
+
+import static cc.quarkus.qcc.runtime.CNative.*;
+
+public class Example
+{
+    @extern
+    public static native int putchar(int arg);
+
+    @export
+    public static int main()
+    {
+        putchar('h');
+        putchar('e');
+        putchar('l');
+        putchar('l');
+        putchar('o');
+        putchar(' ');
+        putchar('w');
+        putchar('o');
+        putchar('r');
+        putchar('l');
+        putchar('d');
+        putchar('\n');
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        // make driver happy
+    }
+
+}
