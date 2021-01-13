9dec7bab2fb30140f6466c4f4916d41e08505082
Galder Zamarreño <galder@redhat.com>
2021-01-13 15:56:27 +0100

    Restore the rest of widening conversions

--- widening/example/Example.java
+++ widening/example/Example.java
@@ -6,125 +6,119 @@ import static cc.quarkus.qcc.runtime.CNative.*;
 
 public class Example
 {
+    private short low;
+
     @extern
     public static native int putchar(int arg);
 
     @export
     public static int main()
     {
-        {
-//            putchar(2147483646 - ((int)(float) 2147483646) == -1 ? '1' : '0');
+        putchar('b');
+        putchar('y');
+        putchar('t');
+        putchar('e');
+        putchar('\n');
 
-            int high = Integer.MAX_VALUE - 1;
-            putchar(high - (int)((float) high) == -1 ? '1' : '0'); // shows 0
-            // putchar(high - (int)((float) high) == -2 ? '1' : '0'); // shows 1
+        {
+            byte low = Byte.MIN_VALUE + 1;
+            byte high = Byte.MAX_VALUE - 1;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (byte)((short) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((short) high) == 0 ? '.' : 'F');
+            putchar(low - (byte)((int) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((int) high) == 0 ? '.' : 'F');
+            putchar(low - (byte)((long) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((long) high) == 0 ? '.' : 'F');
+            putchar(low - (byte)((float) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((float) high) == 0 ? '.' : 'F');
+            putchar(low - (byte)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (byte)((double) high) == 0 ? '.' : 'F');
         }
 
-//        putchar('b');
-//        putchar('y');
-//        putchar('t');
-//        putchar('e');
-//        putchar('\n');
-//
-//        {
-//            byte low = Byte.MIN_VALUE + 1;
-//            byte high = Byte.MAX_VALUE - 1;
-//            putchar(low - low == 0 ? '1' : '0');
-//            putchar(high - high == 0 ? '1' : '0');
-//            putchar(low - (byte)((short) low) == 0 ? '1' : '0');
-//            putchar(high - (byte)((short) high) == 0 ? '1' : '0');
-//            putchar(low - (byte)((int) low) == 0 ? '1' : '0');
-//            putchar(high - (byte)((int) high) == 0 ? '1' : '0');
-//            putchar(low - (byte)((long) low) == 0 ? '1' : '0');
-//            putchar(high - (byte)((long) high) == 0 ? '1' : '0');
-//            putchar(low - (byte)((float) low) == 0 ? '1' : '0');
-//            putchar(high - (byte)((float) high) == 0 ? '1' : '0');
-//            putchar(low - (byte)((double) low) == 0 ? '1' : '0');
-//            putchar(high - (byte)((double) high) == 0 ? '1' : '0');
-//        }
-//
-//        putchar('\n');
-//        putchar('s');
-//        putchar('h');
-//        putchar('o');
-//        putchar('r');
-//        putchar('t');
-//        putchar('\n');
-//
-//        {
-//            short low = Short.MIN_VALUE + 1;
-//            short high = Short.MAX_VALUE - 1;
-//            putchar(low - low == 0 ? '1' : '0');
-//            putchar(high - high == 0 ? '1' : '0');
-//            putchar(low - (short)((int) low) == 0 ? '1' : '0');
-//            putchar(high - (short)((int) high) == 0 ? '1' : '0');
-//            putchar(low - (short)((long) low) == 0 ? '1' : '0');
-//            putchar(high - (short)((long) high) == 0 ? '1' : '0');
-//            putchar(low - (short)((float) low) == 0 ? '1' : '0');
-//            putchar(high - (short)((float) high) == 0 ? '1' : '0');
-//            putchar(low - (short)((double) low) == 0 ? '1' : '0');
-//            putchar(high - (short)((double) high) == 0 ? '1' : '0');
-//        }
-//
-//        putchar('\n');
-//        putchar('c');
-//        putchar('h');
-//        putchar('a');
-//        putchar('r');
-//        putchar('\n');
-//
-//        {
-//            char low = Character.MIN_VALUE + 1;
-//            char high = Character.MAX_VALUE - 1;
-//            putchar(low - low == 0 ? '1' : '0');
-//            putchar(high - high == 0 ? '1' : '0');
-//            putchar(low - (char)((int) low) == 0 ? '1' : '0');
-//            putchar(high - (char)((int) high) == 0 ? '1' : '0');
-//            putchar(low - (char)((long) low) == 0 ? '1' : '0');
-//            putchar(high - (char)((long) high) == 0 ? '1' : '0');
-//            putchar(low - (char)((float) low) == 0 ? '1' : '0');
-//            putchar(high - (char)((float) high) == 0 ? '1' : '0');
-//            putchar(low - (char)((double) low) == 0 ? '1' : '0');
-//            putchar(high - (char)((double) high) == 0 ? '1' : '0');
-//        }
-//
-//        putchar('\n');
-//        putchar('i');
-//        putchar('n');
-//        putchar('t');
-//        putchar('\n');
-//
-//        {
-//            int low = Integer.MIN_VALUE + 1;
-//            int high = Integer.MAX_VALUE - 1;
-//            putchar(low - low == 0 ? '1' : '0');
-//            putchar(high - high == 0 ? '1' : '0');
-//            putchar(low - (int)((long) low) == 0 ? '1' : '0');
-//            putchar(high - (int)((long) high) == 0 ? '1' : '0');
-//            putchar(low - (int)((float) low) == 1 ? '1' : '0'); // loss of precision
-//            putchar(high - (int)((float) high) == -1 ? '1' : '0'); // loss of precision
-//            putchar(low - (int)((double) low) == 0 ? '1' : '0');
-//            putchar(high - (int)((double) high) == 0 ? '1' : '0');
-//        }
-//
-//        putchar('\n');
-//        putchar('l');
-//        putchar('o');
-//        putchar('n');
-//        putchar('g');
-//        putchar('\n');
-//        {
-//            long low = Long.MIN_VALUE + 1;
-//            long high = Long.MAX_VALUE - 1;
-//            putchar(low - low == 0 ? '1' : '0');
-//            putchar(high - high == 0 ? '1' : '0');
-//            putchar(low - (long)((float) low) == 1 ? '1' : '0'); // loss of precision
-//            putchar(high - (long)((float) high) == -1 ? '1' : '0'); // loss of precision
-//            putchar(low - (long)((double) low) == 1 ? '1' : '0'); // loss of precision
-//            putchar(high - (long)((double) high) == -1 ? '1' : '0'); // loss of precision
-//        }
-//
-//        putchar('\n');
+        putchar('\n');
+        putchar('s');
+        putchar('h');
+        putchar('o');
+        putchar('r');
+        putchar('t');
+        putchar('\n');
+
+        {
+            short low = Short.MIN_VALUE + 1;
+            short high = Short.MAX_VALUE - 1;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (short)((int) low) == 0 ? '.' : 'F');
+            putchar(high - (short)((int) high) == 0 ? '.' : 'F');
+            putchar(low - (short)((long) low) == 0 ? '.' : 'F');
+            putchar(high - (short)((long) high) == 0 ? '.' : 'F');
+            putchar(low - (short)((float) low) == 0 ? '.' : 'F');
+            putchar(high - (short)((float) high) == 0 ? '.' : 'F');
+            putchar(low - (short)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (short)((double) high) == 0 ? '.' : 'F');
+        }
+
+        putchar('\n');
+        putchar('c');
+        putchar('h');
+        putchar('a');
+        putchar('r');
+        putchar('\n');
+
+        {
+            char low = Character.MIN_VALUE + 1;
+            char high = Character.MAX_VALUE - 1;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (char)((int) low) == 0 ? '.' : 'F');
+            putchar(high - (char)((int) high) == 0 ? '.' : 'F');
+            putchar(low - (char)((long) low) == 0 ? '.' : 'F');
+            putchar(high - (char)((long) high) == 0 ? '.' : 'F');
+            putchar(low - (char)((float) low) == 0 ? '.' : 'F');
+            putchar(high - (char)((float) high) == 0 ? '.' : 'F');
+            putchar(low - (char)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (char)((double) high) == 0 ? '.' : 'F');
+        }
+
+        putchar('\n');
+        putchar('i');
+        putchar('n');
+        putchar('t');
+        putchar('\n');
+
+        {
+            int low = Integer.MIN_VALUE + 1;
+            int high = Integer.MAX_VALUE - 1;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (int)((long) low) == 0 ? '.' : 'F');
+            putchar(high - (int)((long) high) == 0 ? '.' : 'F');
+            putchar(low - (int)((float) low) == 1 ? '.' : 'F'); // loss of precision
+            putchar(high - (int)((float) high) == -1 ? '.' : 'F'); // loss of precision
+            putchar(low - (int)((double) low) == 0 ? '.' : 'F');
+            putchar(high - (int)((double) high) == 0 ? '.' : 'F');
+        }
+
+        putchar('\n');
+        putchar('l');
+        putchar('o');
+        putchar('n');
+        putchar('g');
+        putchar('\n');
+        {
+            long low = Long.MIN_VALUE + 1;
+            long high = Long.MAX_VALUE - 1;
+            putchar(low - low == 0 ? '.' : 'F');
+            putchar(high - high == 0 ? '.' : 'F');
+            putchar(low - (long)((float) low) == 1 ? '.' : 'F'); // loss of precision
+            putchar(high - (long)((float) high) == -1 ? '.' : 'F'); // loss of precision
+            putchar(low - (long)((double) low) == 1 ? '.' : 'F'); // loss of precision
+            putchar(high - (long)((double) high) == -1 ? '.' : 'F'); // loss of precision
+        }
+
+        putchar('\n');
         return 0;
     }
 
