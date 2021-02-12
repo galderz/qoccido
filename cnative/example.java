308ea23ad6f7f495e8c8644da972caa816874457
Galder Zamarreño <galder@redhat.com>
2021-02-12 17:52:12 +0100

    Exercise BitCast optimization with unsigned numbers

--- cnative/example.java
+++ cnative/example.java
@@ -1,3 +1,5 @@
+import cc.quarkus.qcc.runtime.stdc.Stdint.uint32_t;
+
 import static cc.quarkus.qcc.runtime.CNative.*;
 
 public class example
@@ -8,11 +10,26 @@ public class example
     @export
     public static c_int main(c_int argc, ptr<?> argv)
     {
+        bitCastOptimizationTest();
+        // smokeTest();
+
+        return word(0);
+    }
+
+    // Exercises this optimization:
+    // BitCast(BitCast(a, x), y) -> BitCast(a, y)
+    private static void bitCastOptimizationTest()
+    {
+        final uint32_t foo = word(1234);
+        final float bar = Float.intBitsToFloat(foo.intValue());
+        putchar(1.729E-42f == bar ? word('.') : word('F'));
+    }
+
+    private static void smokeTest()
+    {
         _Float32 foo = word(1.0f);
         _Float32 bar = word(1.0f);
         putchar(foo == bar ? word('.') : word('F'));
-
-        return word(0);
     }
 
     public static void main(String[] args)
