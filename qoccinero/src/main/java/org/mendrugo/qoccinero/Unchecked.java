b0094fd756886c9106a0abce8ab5a87e486b9bc0
Galder Zamarreño <galder@redhat.com>
2021-03-12 16:49:33 +0100

    Make method public

--- qoccinero/src/main/java/org/mendrugo/qoccinero/Unchecked.java
+++ qoccinero/src/main/java/org/mendrugo/qoccinero/Unchecked.java
@@ -3,7 +3,7 @@ package org.mendrugo.qoccinero;
 public class Unchecked
 {
     @SuppressWarnings("unchecked")
-    static <T> T cast(Object o)
+    public static <T> T cast(Object o)
     {
         return (T) o;
     }
