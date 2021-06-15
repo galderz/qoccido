e3a380519006bc78f58810cdfc0fc0f96e5e040d
Galder Zamarreño <galder@redhat.com>
2021-06-15 13:31:06 +0200

    More parameters and better naming for easier distinction

--- scalar-replacement/example.java
+++ scalar-replacement/example.java
@@ -2,24 +2,28 @@ import static org.qbicc.runtime.CNative.*;
 
 public class example
 {
-    final int exampleField;
+    final int fieldA;
+    final int fieldB;
+    final int fieldC;
 
-    public example(int exampleParameter)
+    public example(int a, int b, int c)
     {
-        this.exampleField = exampleParameter;
+        this.fieldA = a;
+        this.fieldB = b;
+        this.fieldC = c;
     }
 
     @extern
     public static native int putchar(int arg);
 
-    static int single(int x)
+    static int single(int a, int b, int c)
     {
-        example obj = new example(x);
-        return obj.exampleField;
+        example obj = new example(a, b, c);
+        return obj.fieldA;
     }
 
     public static void main(String[] args)
     {
-        putchar(single(10) == 10 ? '.' : 'F');
+        putchar(single(10, 20, 30) == 10 ? '.' : 'F');
     }
 }
