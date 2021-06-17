7e9b3ad58fb98880f4378bb35f42ee1abb07bd7d
Galder Zamarreño <galder@redhat.com>
2021-06-17 14:22:02 +0200

    Second escape analysis example, with local variable reassignment

---
+++ ea-02/example.java
@@ -0,0 +1,44 @@
+import static org.qbicc.runtime.CNative.*;
+
+/**
+ * An example to achieve stack allocation of 'example' object.
+ *
+ * From:
+ *   %L0 = call i8 addrspace(1)* (i8 addrspace(1)*, i64, i32) @exact.org.qbicc.runtime.gc.nogc.NoGcHelpers.allocate.ref.1.class.java-lang-Object.2.s64.s32(i8 a
+ * ddrspace(1)* %thr0, i64 16, i32 8), !dbg !34
+ *
+ * To:
+ *   %L0 = alloca %T.example, i32 1, align 8
+ *
+ * Note:
+ * With further analysis, the object access can be strength reduced,
+ * and the creation of object may be eliminated.
+ */
+public class example
+{
+    final int fieldA;
+    final int fieldB;
+    final int fieldC;
+
+    public example(int a, int b, int c)
+    {
+        this.fieldA = a;
+        this.fieldB = b;
+        this.fieldC = c;
+    }
+
+    @extern
+    public static native int putchar(int arg);
+
+    static int single(int a, int b, int c)
+    {
+        example exampleVar = new example(a, b, c);
+        example tmp = exampleVar;
+        return tmp.fieldA;
+    }
+
+    public static void main(String[] args)
+    {
+        putchar(single(10, 20, 30) == 10 ? '.' : 'F');
+    }
+}
