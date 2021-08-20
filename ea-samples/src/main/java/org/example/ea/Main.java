975c077d1fc929a32aaca4b984fea5ba39bc7f0d
Galder Zamarreño <galder@redhat.com>
2021-08-20 14:06:46 +0200

    Add a end of line at the end

--- ea-samples/src/main/java/org/example/ea/Main.java
+++ ea-samples/src/main/java/org/example/ea/Main.java
@@ -4,6 +4,9 @@ import org.example.ea.samples.EASample_01_Basic;
 import org.example.ea.samples.EASample_02_StaticAssignment;
 import org.example.ea.samples.EASample_03_ParameterEscape;
 import org.example.ea.samples.EASample_04_ReturnEscape;
+import org.qbicc.runtime.CNative;
+
+import static org.qbicc.runtime.CNative.*;
 
 public class Main
 {
@@ -13,5 +16,9 @@ public class Main
         EASample_02_StaticAssignment.main(args);
         EASample_03_ParameterEscape.main(args);
         EASample_04_ReturnEscape.main(args);
+        putchar('\n');
     }
+
+    @extern
+    public static native int putchar(int arg);
 }
