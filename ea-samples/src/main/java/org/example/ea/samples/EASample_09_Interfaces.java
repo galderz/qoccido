011eee21d32cb8150d98732060c596dd4f54736d
Galder Zamarreño <galder@redhat.com>
2021-10-29 13:45:51 +0200

    Complete monomporphic interface sample

--- ea-samples/src/main/java/org/example/ea/samples/EASample_09_Interfaces.java
+++ ea-samples/src/main/java/org/example/ea/samples/EASample_09_Interfaces.java
@@ -2,45 +2,48 @@ package org.example.ea.samples;
 
 import org.example.ea.Main;
 
-public class EASample_09_Interface
+public class EASample_09_Interfaces
 {
     /*
      * This sample showcases the effects of calls on interfaces,
      * and the impact implementations have on whether values escape or not.
-     *
-     * In the global escape sample,
-     * an interface has 2 implementations,
-     * one of which causes the argument to the interface method to global escape.
-     * As a result, calling that interface should pessimistically assume that the argument is always global escape.
-     *
-     * TODO no escape sample
      */
 
     public static void main(String[] args)
     {
-        globalEscape();
+        /*
+         * In the monomorphic interface sample,
+         * there are 2 implementations for the interface,
+         * one of which causes the argument to the interface method to global escape.
+         * As a result, calling that interface should pessimistically assume that the argument is always global escape.
+         */
+        monomorphicEscape();
+
+        // TODO polymorphic
+
+        // TODO no escape
     }
 
     static boolean isInfinite(int x, int y)
     {
         final Point point = new Point(x, y);
         final IsInfinite isInfinite = new IsInfinite();
-        return test1(point, isInfinite);
+        return testPoint(point, isInfinite);
     }
 
     static boolean isZero(int x, int y)
     {
         final Point point = new Point(x, y);
         final IsZero isZero = new IsZero();
-        return test1(point, isZero);
+        return testPoint(point, isZero);
     }
 
-    static boolean test1(Point p, Function1<Point, Boolean> predicate)
+    static boolean testPoint(Point p, PointPredicate predicate)
     {
-        return predicate.apply1(p);
+        return predicate.test1(p);
     }
 
-    private static void globalEscape()
+    private static void monomorphicEscape()
     {
         Main.print(!isInfinite(0, 0) ? '.' : 'F');
         Main.print(isInfinite(Integer.MAX_VALUE, Integer.MAX_VALUE) ? '.' : 'F');
@@ -48,21 +51,26 @@ public class EASample_09_Interface
         Main.print(isZero(0, 0) ? '.' : 'F');
     }
 
-    static final class IsInfinite implements Function1<Point, Boolean>
+    interface PointPredicate
+    {
+        Boolean test1(Point t1);
+    }
+
+    static final class IsInfinite implements PointPredicate
     {
         @Override
-        public Boolean apply1(Point p)
+        public Boolean test1(Point p)
         {
             return p.x == Integer.MAX_VALUE && p.y == Integer.MAX_VALUE;
         }
     }
 
-    static final class IsZero implements Function1<Point, Boolean>
+    static final class IsZero implements PointPredicate
     {
         static Point point;
 
         @Override
-        public Boolean apply1(Point p)
+        public Boolean test1(Point p)
         {
             point = p;
             return point.x == 0 && point.y == 0;
@@ -72,17 +80,12 @@ public class EASample_09_Interface
     static final class Point
     {
         final int x;
+
         final int y;
 
         Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
-    }
-
-    @FunctionalInterface
-    interface Function1<T1, R>
-    {
-        R apply1(T1 t1);
     }
 }
