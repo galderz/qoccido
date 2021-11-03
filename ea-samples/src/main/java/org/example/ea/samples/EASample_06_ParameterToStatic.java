02ba4c9f5510eaac0edcb15e4152dfb6773a6e29
Galder Zamarreño <galder@redhat.com>
2021-11-03 11:22:23 +0100

    Refactor for easier separation of different situations

--- ea-samples/src/main/java/org/example/ea/samples/EASample_06_ParameterToStatic.java
+++ ea-samples/src/main/java/org/example/ea/samples/EASample_06_ParameterToStatic.java
@@ -8,55 +8,72 @@ public class EASample_06_ParameterToStatic
      * This sample showcases the effects of assigning a parameter object into a static field.
      */
 
-    static A misc;
-    static A only;
-
-    static int setAndWrite_0()
-    {
-        // New instance of A assigned to variable `a`.
-        final A a = new A();
-        a.aField = 10;
-        // `a` variable passed as parameter.
-        return setAndWrite_1(a);
-        // When the method returns, `a` can no longer be considered not escape,
-        // because within the method it was assigned to a static variable,
-        // and hence it's now considered to global escape.
-    }
-
-    static int setAndWrite_1(A a)
-    {
-        a.next = new A();
-        a.next.aField = 11;
-
-        // Static assignment, so `a` goes from argument escape to global escape.
-        // In turn, the A instance assigned to `a.next` goes from not escape to global escape.
-        misc = a;
-
-        return a.next.aField;
-    }
-
-    static int setOnly_0()
-    {
-        // New instance of A assigned to variable `a`.
-        final A a = new A();
-        // `a` variable passed as parameter.
-        return setOnly_1(a);
-        // When the method returns, `a` can no longer be considered not escape,
-        // because within the method it was assigned to a static variable,
-        // and hence it's now considered to global escape.
-    }
-
-    static int setOnly_1(A a)
-    {
-        // Static assignment, so `a` goes from argument escape to global escape.
-        only = a;
-        return a.aField;
-    }
-
     public static void main(String[] args)
     {
-        Main.print(setAndWrite_0() == 11 ? '.' : 'F');
-        Main.print(setOnly_0() == 0 ? '.' : 'F');
+        AssignWrite.main();
+        AssignOnly.main();
+    }
+
+    static class AssignWrite
+    {
+        static A aw;
+
+        private static void main()
+        {
+            Main.print(assignWrite() == 11 ? '.' : 'F');
+        }
+
+        static int assignWrite()
+        {
+            // New instance of A assigned to variable `a`.
+            final A a = new A();
+            a.aField = 10;
+            // `a` variable passed as parameter.
+            return doAssignWrite(a);
+            // When the method returns, `a` can no longer be considered not escape,
+            // because within the method it was assigned to a static variable,
+            // and hence it's now considered to global escape.
+        }
+
+        static int doAssignWrite(A a)
+        {
+            a.next = new A();
+            a.next.aField = 11;
+
+            // Static assignment, so `a` goes from argument escape to global escape.
+            // In turn, the A instance assigned to `a.next` goes from not escape to global escape.
+            aw = a;
+
+            return a.next.aField;
+        }
+    }
+
+    static class AssignOnly
+    {
+        static A ao;
+
+        private static void main()
+        {
+            Main.print(assignOnly() == 0 ? '.' : 'F');
+        }
+
+        static int assignOnly()
+        {
+            // New instance of A assigned to variable `a`.
+            final A a = new A();
+            // `a` variable passed as parameter.
+            return doAssignOnly(a);
+            // When the method returns, `a` can no longer be considered not escape,
+            // because within the method it was assigned to a static variable,
+            // and hence it's now considered to global escape.
+        }
+
+        static int doAssignOnly(A a)
+        {
+            // Static assignment, so `a` goes from argument escape to global escape.
+            ao = a;
+            return a.aField;
+        }
     }
 
     public static class A
