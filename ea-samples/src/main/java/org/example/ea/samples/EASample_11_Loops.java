bd05baa062e191de69253efb8a2109edbfb062c0
Galder Zamarreño <galder@redhat.com>
2021-11-24 16:52:18 +0100

    Initial loop work

---
+++ ea-samples/src/main/java/org/example/ea/samples/EASample_11_Loops.java
@@ -0,0 +1,24 @@
+package org.example.ea.samples;
+
+import org.example.ea.Main;
+
+public class EASample_11_Loops
+{
+    public static void main(String[] args)
+    {
+        
+    }
+
+    static class WhileLoops
+    {
+//        public static void main()
+//        {
+//            Main.print(factorial(6) == 720 ? '.' : 'F');
+//        }
+//
+//        static int factorial(int number)
+//        {
+//
+//        }
+    }
+}
