c63f8ce957753fc146d45f2dd09463ef34c5ae47
Galder Zamarreño <galder@redhat.com>
2021-12-13 11:32:25 +0100

    Refactor to put all ea projects under one directory

---
+++ ea/ea-jmh/src/test/java/org/example/ea/EscapeAnalysisTest.java
@@ -0,0 +1,13 @@
+package org.example.ea;
+
+import org.junit.jupiter.api.Test;
+
+public class EscapeAnalysisTest
+{
+    @Test
+    public void test000()
+    {
+        System.out.println("hello");
+        EASample_01_Basic_Test.of().run();
+    }
+}
